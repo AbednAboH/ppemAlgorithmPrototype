484a41d5e3e4c3fef62ba867a096b8bcfc5ee30f
Abed <freazer122@gmail.com>
2023-05-17 16:18:47 +0300

    fixed seal library , can be used now
    fixed some of the basic code to be compatible with the final code structure , not all is done , need further optimizations on actual code .. for the BVF , seal was done through https://github.com/Huelse/SEAL-Python.git , problems were solved when all the relevant libraries for visual studio were installed , after creating the actual library using  a wrapper in python the .pyd file should be placed in the directory of the DLL libraries of the python version you are using , c:\....\pythonXY\DLL .
    XY is the version of python you are using .
    if you get an error when trying to run setup from SEAL-Python, most likely something isn't installed in your visual studio version .

--- PPEM.py
+++ PPEM.py
@@ -10,7 +10,8 @@ import seal
 class PPEM(algortithem):
     def __init__(self, n, inputType, inputDimentions, max_iter, number_of_clustures, eps=1e-4, input=None):
         super(PPEM, self).__init__(n, inputType, inputDimentions, max_iter, number_of_clustures, eps=1e-4, input=None)
-
+        self.public_key=None
+        self.private_key=None
     def encrypt_data(self,public_key, data):
         """Encrypt the data using BFV encryption"""
         context = public_key.context()
@@ -30,34 +31,34 @@ class PPEM(algortithem):
         return encoder.decode(decrypted_data)
     # todo cange the signiture as you don't need this one , once the library is installed you will be able \
     #  to see if you wrote code that isn't good enough
-    def eStep(self,X, means, covariances, weights, public_key):
+    def eStep(self):
         """Calculate the E-step of the EM algorithm"""
-        n_samples, n_features = X.shape
-        n_components = len(weights)
+        n_samples, n_features = self.n_inputs.shape
+        n_components = len(self.responsibilities)
 
         # Calculate the probabilities of each sample belonging to each component
         log_probabilities = np.zeros((n_samples, n_components))
         for i in range(n_components):
-            mean = means[i]
-            covariance = covariances[i]
-            weight = weights[i]
+            mean = self.means[i]
+            covariance = self.covariances[i]
+            responsibilities = self.responsibilities[i]
             covariance_det = np.linalg.det(covariance)
             covariance_inv = np.linalg.inv(covariance)
             coefficient = 1.0 / np.sqrt((2 * np.pi) ** n_features * covariance_det)
             for j in range(n_samples):
-                x = X[j]
-                x_encrypted = encrypt_data(public_key, x)
-                mean_encrypted = encrypt_data(public_key, mean)
-                covariance_inv_encrypted = encrypt_data(public_key, covariance_inv)
+                x = self.responsibilities[j]
+                x_encrypted = self.encrypt_data(self.public_key, x)
+                mean_encrypted = self.encrypt_data(self.public_key, mean)
+                covariance_inv_encrypted = self.encrypt_data(self.public_key, covariance_inv)
                 quadratic_form = (x_encrypted - mean_encrypted).dot(covariance_inv_encrypted).dot(
                     x_encrypted - mean_encrypted)
-                log_probabilities[j, i] = np.log(weight) - 0.5 * np.log(covariance_det) - 0.5 * quadratic_form
+                log_probabilities[j, i] = np.log(responsibilities) - 0.5 * np.log(covariance_det) - 0.5 * quadratic_form
         log_likelihood = np.sum(log_probabilities, axis=1)
         log_probabilities -= log_likelihood[:, np.newaxis]
         probabilities = np.exp(log_probabilities)
         return log_likelihood, probabilities
 
-    def mstep(self,X, gamma, k, n, m, public_key, secret_key):
+    def mstep(self, responsibilities, public_key, secret_key):
         # Initialize variables
         mu = []
         sigma = []
@@ -66,24 +67,24 @@ class PPEM(algortithem):
         ciphertexts = []
 
         # Calculate the new means, covariances, and mixing coefficients
-        for i in range(k):
+        for i in range(self.k):
             # Calculate the new mixing coefficient
             pi_i = (np.sum(gamma[:, i]) + epsilon) / n
             pi.append(pi_i)
 
             # Calculate the new mean
-            mu_i = np.sum(gamma[:, i].reshape(-1, 1) * X, axis=0) / (np.sum(gamma[:, i]) + epsilon)
+            mu_i = np.sum(gamma[:, i].reshape(-1, 1) * responsibilities, axis=0) / (np.sum(gamma[:, i]) + epsilon)
             mu.append(mu_i)
 
             # Calculate the new covariance
-            diff = X - mu_i
+            diff = responsibilities - mu_i
             cov_i = np.dot((gamma[:, i].reshape(-1, 1) * diff).T, diff) / (np.sum(gamma[:, i]) + epsilon)
             sigma.append(cov_i)
 
             # Encrypt the cluster parameters
-            mu_i_encrypted = bfv_encrypt(mu_i, public_key)
-            cov_i_encrypted = bfv_encrypt(cov_i, public_key)
-            pi_i_encrypted = bfv_encrypt(pi_i, public_key)
+            mu_i_encrypted = self.encrypt_data(mu_i, public_key)
+            cov_i_encrypted = self.encrypt_data(cov_i, public_key)
+            pi_i_encrypted = self.encrypt_data(pi_i, public_key)
             ciphertexts.append((mu_i_encrypted, cov_i_encrypted, pi_i_encrypted))
 
         return ciphertexts, mu, sigma, pi
